976e31ab888a57ce27f673c1c4f1e6641a53cb3b
ManojF <manojf@ManojF-Temp.local>
2021-09-01 03:03:30 +0530

    Added fib generator code

--- handler.js
+++ handler.js
@@ -1,14 +1,28 @@
 'use strict';
 
+// Calculating the fibonacci value
+const fib = (n) => {
+  if (n <= 1) return n;
+  return fib(n - 1) + fib(n - 2);
+}
+
 module.exports.generate = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Hello from fib generator',
-      },
-      null,
-      2
-    ),
-  };
+  let output = 0;
+  try {
+    const number = parseInt(event.pathParameters.number);
+    if (number) {
+      output = fib(number);
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        result: output
+      })
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error)
+    }
+  }
 };
