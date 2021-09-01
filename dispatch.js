c3ebc07adf5477a27b2a335a29b8f026205f757c
ManojF <manojf@ManojF-Temp.local>
2021-09-01 11:54:18 +0530

    logz.io integration

--- dispatch.js
+++ dispatch.js
@@ -1,17 +1,22 @@
 const zlib = require('zlib');
 const aws = require("aws-sdk");
 const ses = new aws.SES({ region: "us-east-1" });
+const logzio = require('logzio-nodejs');
+
+const logger = logzio.createLogger({
+    token: process.env.logzio_token,
+    type: 'CRITICAL'
+});
 
 const generateEmailContent = (data) => {
-    let parsedData = JSON.parse(data.split('INFO')[1].trim());
     let logData = "<br/><h2><u>Application Logs</u></h2>";
-    logData += `<p style='color: red; font-size: 16px;'><b>Status:</b>${parsedData.type}</p>`;
-    logData += `<p style='font-size: 14px;'><b>Stage:</b>${parsedData.context.stage}</p>`;
-    logData += `<p style='font-size: 14px;'><b>IP:</b>${parsedData.context.sourceIp}</p>`;
-    logData += `<p style='font-size: 14px;'><b>URL Path:</b>${parsedData.context.path}</p>`;
-    logData += `<p style='font-size: 14px;'><b>HTTP Method:</b>${parsedData.context.httpMethod}</p>`;
-    logData += `<p style='font-size: 14px;'><b>Message:</b>${parsedData.message}</p>`;
-    logData += `<p style='font-size: 14px;'><b>Callstack:</b>${parsedData.callstack || 'N/A'}</p>`;
+    logData += `<p style='color: red; font-size: 16px;'><b>Status:</b>${data.type}</p>`;
+    logData += `<p style='font-size: 14px;'><b>Stage:</b>${data.context.stage}</p>`;
+    logData += `<p style='font-size: 14px;'><b>IP:</b>${data.context.sourceIp}</p>`;
+    logData += `<p style='font-size: 14px;'><b>URL Path:</b>${data.context.path}</p>`;
+    logData += `<p style='font-size: 14px;'><b>HTTP Method:</b>${data.context.httpMethod}</p>`;
+    logData += `<p style='font-size: 14px;'><b>Message:</b>${data.message}</p>`;
+    logData += `<p style='font-size: 14px;'><b>Callstack:</b>${data.callstack || 'N/A'}</p>`;
 
     let subject = `Critical Alert - Fib generator API`;
 
@@ -48,18 +53,22 @@ const generateEmailContent = (data) => {
     return emailContent;
 }
 
-exports.handler = function (input, context) {
+exports.handler = (input, context) => {
     let payload = Buffer.from(input.awslogs.data, 'base64');
-    zlib.gunzip(payload, function (e, result) {
+    zlib.gunzip(payload, (e, result) => {
         if (e) {
             context.fail(e);
         } else {
             result = JSON.parse(result.toString('ascii'));
             const latestLogEvent = result.logEvents[0];
-            ses.sendEmail(generateEmailContent(latestLogEvent.message), (err, data) => {
+            let data = JSON.parse(latestLogEvent.message.split('INFO')[1].trim());
+
+            // Send Email
+            ses.sendEmail(generateEmailContent(data), (err, result) => {
                 if (err) console.log(err);
                 else {
-                    console.log(data);
+                    // Log in logz.io
+                    logger.log(data);
                     context.succeed();
                 }
             });
